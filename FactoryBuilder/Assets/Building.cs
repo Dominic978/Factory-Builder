941d1f963bf9b295e59e935ee7f3e9f990f0bb87
Dominic978 <mdominic978@gmail.com>
2025-06-25 15:13:16 -0700

    Forgot to hit a checkmark on files
    files weren't included in last commit
    -oopsie daisy

--- FactoryBuilder/Assets/Building.cs
+++ FactoryBuilder/Assets/Building.cs
@@ -5,7 +5,6 @@ public class Building : MonoBehaviour
     public Vector2Int gridPos;//set at init
     public Vector2Int size;   //set at init
 
-    private void Update()
     public void Init(Vector2Int gridPos, Vector2Int size)
     {
         this.gridPos = gridPos;
